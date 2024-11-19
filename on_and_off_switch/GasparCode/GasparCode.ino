e25b13044abfeabe8bf62cb1b94a761fd2f3366a
Swinubo <gasparmendes66@gmail.com>
2024-11-18 21:42:31 -0500

    ehhehehe on and off works

--- on_and_off_switch/GasparCode/GasparCode.ino
+++ on_and_off_switch/GasparCode/GasparCode.ino
@@ -6,7 +6,7 @@ bool LEDvar = false;
 void setup() {
   // put your setup code here, to run once:
   pinMode(2, OUTPUT);
-  pinMode(4, INPUT);
+  pinMode(4, INPUT_PULLUP);
 
   // Ensure LED starts off
   digitalWrite(2, LOW);
@@ -15,12 +15,33 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  if (digitalRead(pin4) == HIGH) {
+  bool pressed = false;
+  while(!pressed)
+  {
+    if (digitalRead(pin4) == LOW && LEDvar == false) {
+      LEDvar = true;
+      pressed = true;
+    }
+    else if (digitalRead(pin4) == LOW && LEDvar == true) {
+      LEDvar = false;
+      pressed = true;
+    }
+  }
+
+  /*if (digitalRead(pin4) == LOW && LEDvar == false) {
     LEDvar = true;
   }
+  else if (digitalRead(pin4) == LOW && LEDvar == true) {
+    LEDvar = false;
+  }*/
 
   if (LEDvar == true)
   {
     digitalWrite(pin2, HIGH);
   }
+  if (LEDvar == false)
+  {
+    digitalWrite(pin2, LOW);
+  }
+  delay(1000);
 }
